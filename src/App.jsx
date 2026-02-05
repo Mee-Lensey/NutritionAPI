174f96a1d74ce572ff6d18f49c5169b237a49206
Mee-Lensey <148074806+Mee-Lensey@users.noreply.github.com>
2026-02-05 10:58:29 -0500

    foods html

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,7 @@ function App() {
       <div>
         
         </a>
-        <a href="https://react.dev" target="_blank">  
-         <img src={reactLogo} className="logo react" alt="React logo" />
+       <></>
         </a>
       </div>
       <h1>Vite + React</h1>
